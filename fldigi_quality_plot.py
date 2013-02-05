94c500010fd566ba6afd08f1738bd11fa5fa505e
Mark Jessop <darkside@Marks-MacBook-Pro-4.local>
2013-02-05 20:05:09 +1030

    Hackish dynamic plotting working, using pylab.

--- fldigi_quality_plot.py
+++ fldigi_quality_plot.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python
 
-# 	FLDIGI Channel Quality Grapher
+#	FLDIGI Channel Quality Grapher
 #
 #	This program plots the modem signal quality value, as reported by fldigi.
 #	Information is collected from fldigi using its XMLRPC interface:
 #	http://www.w1hkj.com/FldigiHelp-3.21/xmlrpc-control.html
-# 	
-# 	Copyright (C) 2013 Mark Jessop <mark.jessop@adelaide.edu.au>
+#	
+#	Copyright (C) 2013 Mark Jessop <mark.jessop@adelaide.edu.au>
 # 
-#     This program is free software: you can redistribute it and/or modify
-#     it under the terms of the GNU General Public License as published by
-#     the Free Software Foundation, either version 3 of the License, or
-#     (at your option) any later version.
+#	  This program is free software: you can redistribute it and/or modify
+#	  it under the terms of the GNU General Public License as published by
+#	  the Free Software Foundation, either version 3 of the License, or
+#	  (at your option) any later version.
 # 
-#     This program is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU General Public License for more details.
+#	  This program is distributed in the hope that it will be useful,
+#	  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+#	  GNU General Public License for more details.
 # 
-#     For a full copy of the GNU General Public License, 
-#     see <http://www.gnu.org/licenses/>.
+#	  For a full copy of the GNU General Public License, 
+#	  see <http://www.gnu.org/licenses/>.
 #
 #
 
 from xmlrpclib import ServerProxy, Error
 import socket,sys,time
+import pylab as pl
+import numpy as np
 
 # Open a connection to the fldigi XMLRPC server
 fldigi = ServerProxy("http://localhost:7362")
@@ -38,8 +40,29 @@ except socket.error as v:
 	sys.exit(1)
 
 
+data = []
+# Setup plot
+pl.ion()
+fig = pl.figure()
+ax = fig.add_subplot(1,1,1)
+
+
+# Plot as fast as we can
 while True:
-	current_time = time.strftime("%Y-%m-%d-%H%M%S: ",time.gmtime())
-	current_quality = fldigi.modem.get_quality();
-	print current_time + str(current_quality)
-	time.sleep(0.25)
+	# Get quality data
+	current_quality = fldigi.modem.get_quality()
+	data.append(current_quality)
+	
+	# Limit to 100 data points
+	data = data[-100:]
+	# Generate X scale
+	xscale = np.arange(len(data))
+	
+	# Clear and re-plot
+	ax.clear()
+	ax.plot(xscale,data)
+	ax.axis([0,100,0,100])
+	pl.draw()
+
+
+
